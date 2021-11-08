fe827651122484b5accd940f1025cae619a75de0
Liridon Loku <liridonloku@gmail.com>
2021-11-08 15:44:43 +0100

    Switch to class based components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <div>"Hello!"</div>;
-}
+import React, { Component } from "react";
+import Header from "./components/Header";
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
