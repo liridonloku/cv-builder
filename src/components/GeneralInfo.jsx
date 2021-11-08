5b36b3c29ae8078deae8aee7a87e77de159aec6a
Liridon Loku <liridonloku@gmail.com>
2021-11-08 17:05:07 +0100

    Adjust general info display

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -4,17 +4,20 @@ export class GeneralInfo extends Component {
   render() {
     return (
       <div style={generalInfoStyle}>
-        <h3>{this.props.info.name}</h3>
-        <p>{this.props.info.email}</p>
-        <p>{this.props.info.phone}</p>
+        <h3>Full name: {this.props.info.name}</h3>
+        <p>email: {this.props.info.email}</p>
+        <p>phone no.: {this.props.info.phone}</p>
       </div>
     );
   }
 }
 
 const generalInfoStyle = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "5px",
   margin: "5px 10px",
-  boxShadow: "0 0 3px gray",
+  border: "1px solid black",
 };
 
 export default GeneralInfo;
